c06e40635daac51ffc61588c3b54a9ef884205b2
Merrick Bautista <merrickbautista@gmail.com>
2021-12-06 08:50:38 -0600

    Operate function done

--- calc.js
+++ calc.js
@@ -5,3 +5,9 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 
 const divide = (a,b) => a / b;
+
+const operate = (operation,a,b) => operation(a,b);
+
+console.log(add(2,7));
+console.log(operate(add,2,7));
+console.log(operate(subtract,9,3));
